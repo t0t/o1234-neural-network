a1a76e16769fa89a7724a494851c966c0028fed7
sergio fores <sergiofores@gmail.com>
2025-02-23 18:54:33 +0100

    imagen svg

--- src/main.js
+++ src/main.js
@@ -48,22 +48,17 @@ document.querySelector('#app').innerHTML = `
               <text x="4700.642" y="1359.5" class="text-style">aporta marco base</text>
 
               <g id="cell-BpaLnpxe32i0slQv463X-20">
-                <rect x="4740" y="1180.5" width="101" height="26" class="rect-style"/>
-                <text x="4741.5" y="1195.5" class="text-style">Deﬁne importancia del input...</text>
+                <text x="4745" y="1195.5" class="text-style">Define importancia del input...</text>
               </g>
 
               <g id="cell-BpaLnpxe32i0slQv463X-24">
-                <rect x="4739.5" y="1117" width="91" height="10" class="rect-style"/>
-                <text x="4745.988" y="1123.5" class="text-style">INFO ingresa al sistema.</text>
+                <text x="4745" y="1125.5" class="text-style">INFO ingresa al sistema</text>
               </g>
 
               <g id="cell-gythRNJaE8L_uZdejeKm-5">
                 <path d="M4794.37,1080.72L4907.07,929.74" class="path-style"/>
                 <path d="M4912.01,923.13L4909.84,935.23L4907.07,929.74L4901.02,928.65L4912.01,923.13Z" class="path-style"/>
               </g>
-
-              <rect x="4859" y="1460" width="99" height="10" class="rect-style"/>
-
               <g id="cell-gythRNJaE8L_uZdejeKm-19">
                 <path d="M4932.5,1415L4944.21,943.6" class="path-style"/>
                 <path d="M4944.4,936.1L4949.15,946.22L4944.21,943.6L4939.15,945.97L4944.4,936.1Z" class="path-style"/>
@@ -97,53 +92,43 @@ document.querySelector('#app').innerHTML = `
                 <text x="4938.383" y="888.5" class="text-style">3...</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-45" class="path-style">
-                <path d="M4761,1069.25L4761,1079.75C4761,1080.99 4759.99,1082 4758.75,1082L4701.25,1082C4700.01,1082 4699,1080.99 4699,1079.75L4699,1069.25C4699,1068.01 4700.01,1067 4701.25,1067L4758.75,1067C4759.99,1067 4761,1068.01 4761,1069.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-45">
                 <text x="4703.619" y="1076.5" class="text-style">Ingesta de datos</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-52" class="path-style">
-                <path d="M4859,1298.25L4859,1308.75C4859,1309.99 4857.99,1311 4856.75,1311L4799.25,1311C4798.01,1311 4797,1309.99 4797,1308.75L4797,1298.25C4797,1297.01 4798.01,1296 4799.25,1296L4856.75,1296C4857.99,1296 4859,1297.01 4859,1298.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-52">
                 <text x="4807.266" y="1305.5" class="text-style">Tokenización</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-53" class="path-style">
-                <path d="M4963,1507.25L4963,1517.75C4963,1518.99 4961.99,1520 4960.75,1520L4903.25,1520C4902.01,1520 4901,1518.99 4901,1517.75L4901,1507.25C4901,1506.01 4902.01,1505 4903.25,1505L4960.75,1505C4961.99,1505 4963,1506.01 4963,1507.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-53">
                 <text x="4912.043" y="1514.5" class="text-style">Embeddings</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-54" class="path-style">
-                <path d="M5078,835.75L5078,846.25C5078,847.492 5076.99,848.5 5075.75,848.5L4996.25,848.5C4995.01,848.5 4994,847.492 4994,846.25L4994,835.75C4994,834.508 4995.01,833.5 4996.25,833.5L5075.75,833.5C5076.99,833.5 5078,834.508 5078,835.75Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-54">
                 <text x="5000.348" y="842.5" class="text-style">Ajuste de Temperatura</text>
               </g>
 
               <g id="cell-gythRNJaE8L_uZdejeKm-56">
-                <path transform="matrix(0.707107,-0.707107,0.707107,0.707107,720.942,3703.51)" d="M4874,976.25L4874,986.75C4874,987.992 4872.99,989 4871.75,989L4790.25,989C4789.01,989 4788,987.992 4788,986.75L4788,976.25C4788,975.008 4789.01,974 4790.25,974L4871.75,974C4872.99,974 4874,975.008 4874,976.25Z" class="path-style"/>
                 <text transform="matrix(0.707107,-0.707107,0.707107,0.707107,4832.27,982.768)" class="text-style">Multiplicación por pesos</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-57" class="path-style">
-                <path d="M5118,1322.25L5118,1332.75C5118,1333.99 5116.99,1335 5115.75,1335L5058.25,1335C5057.01,1335 5056,1333.99 5056,1332.75L5056,1322.25C5056,1321.01 5057.01,1320 5058.25,1320L5115.75,1320C5116.99,1320 5118,1321.01 5118,1322.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-57">
                 <text x="5060.231" y="1329.5" class="text-style">Backpropagation</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-59" class="path-style">
-                <path d="M4448.06,1100.13L4448.06,1110.63C4448.06,1111.87 4447.05,1112.88 4445.81,1112.88L4388.31,1112.88C4387.07,1112.88 4386.06,1111.87 4386.06,1110.63L4386.06,1100.13C4386.06,1098.89 4387.07,1097.88 4388.31,1097.88L4445.81,1097.88C4447.05,1097.88 4448.06,1098.89 4448.06,1100.13Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-59">
                 <text x="4397.436" y="1106.5" class="text-style">Persistencia</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-62" class="path-style">
-                <path d="M5145,1033.25L5145,1043.75C5145,1044.99 5143.99,1046 5142.75,1046L5063.25,1046C5062.01,1046 5061,1044.99 5061,1043.75L5061,1033.25C5061,1032.01 5062.01,1031 5063.25,1031L5142.75,1031C5143.99,1031 5145,1032.01 5145,1033.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-62">
                 <text x="5073.123" y="1040.5" class="text-style">Decisión de la Red</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-65" class="path-style">
-                <path d="M5252,1264.25L5252,1274.75C5252,1275.99 5250.99,1277 5249.75,1277L5200.25,1277C5199.01,1277 5198,1275.99 5198,1274.75L5198,1264.25C5198,1263.01 5199.01,1262 5200.25,1262L5249.75,1262C5250.99,1262 5252,1263.01 5252,1264.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-65">
                 <text x="5196.873" y="1271.5" class="text-style">Generación de l...</text>
               </g>
 
-              <g id="cell-gythRNJaE8L_uZdejeKm-66" class="path-style">
-                <path d="M5056,1238.25L5056,1248.75C5056,1249.99 5054.99,1251 5053.75,1251L4996.25,1251C4995.01,1251 4994,1249.99 4994,1248.75L4994,1238.25C4994,1237.01 4995.01,1236 4996.25,1236L5053.75,1236C5054.99,1236 5056,1237.01 5056,1238.25Z"/>
+              <g id="cell-gythRNJaE8L_uZdejeKm-66">
                 <text x="4993.374" y="1245.5" class="text-style">Retroalimentación...</text>
               </g>
             </g>
